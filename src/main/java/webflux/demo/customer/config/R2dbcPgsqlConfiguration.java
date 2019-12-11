606d85a9bafba9fd75c85fc78878817162143ea8
Nguyễn Văn Hoàng (VSM-VNCTBGĐ-TTNCTB) <HoangNV30@vingroup.net>
2019-12-11 14:31:32 +0700

    r2dbc

---
+++ src/main/java/webflux/demo/customer/config/R2dbcPgsqlConfiguration.java
@@ -0,0 +1,25 @@
+package webflux.demo.customer.config;
+
+//not used
+//@Configuration
+//@EnableR2dbcRepositories(basePackages = "com.vinsmart.iot.smarthome.backend.user.repository")
+public class R2dbcPgsqlConfiguration{// extends AbstractR2dbcConfiguration {
+//
+//	@Value("${spring.data.postgres.host}")
+//	private String host;
+//	@Value("${spring.data.postgres.port}")
+//	private int port;
+//	@Value("${spring.data.postgres.database}")
+//	private String databaseName;
+//	@Value("${spring.data.postgres.username}")
+//	private String username;
+//	@Value("${spring.data.postgres.password}")
+//	private String password;
+//
+//	@Bean
+//	@Override
+//	public PostgresqlConnectionFactory connectionFactory() {
+//		return new PostgresqlConnectionFactory(PostgresqlConnectionConfiguration.builder().host(host).database(databaseName)
+//				.username(username).password(password).port(port).build());
+//	}
+}
--- src/main/java/webflux/demo/customer/config/SwaggerConfiguration.java
+++ src/main/java/webflux/demo/customer/config/SwaggerConfiguration.java
@@ -4,6 +4,12 @@ import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurationSupport;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,7 +21,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfiguration extends WebFluxConfigurationSupport{
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
@@ -24,11 +30,24 @@ public class SwaggerConfiguration {
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfo("MSA#customer APIs",
-				"Spring Web demo", "1.0.0-DEV",
+		return new ApiInfo("MSA#customer APIs", "Spring Web demo", "1.0.0-DEV",
 				"https://hoangnv.vn/termofservice_license",
-				new Contact("Hoangnv", "https://hoangnv.vn",
-						"api_cs@hoangnv.com.vn"),
-				"© VDS", "https://hoangnv.vn/termofservice_license", Collections.emptyList());
+				new Contact("Hoangnv", "https://hoangnv.vn", "api_cs@hoangnv.com.vn"), "© VDS",
+				"https://hoangnv.vn/termofservice_license", Collections.emptyList());
 	}
+
+	@Bean
+	  public Jackson2JsonEncoder jackson2JsonEncoder(ObjectMapper mapper) {
+	      return new Jackson2JsonEncoder(mapper);
+	  }
+
+	@Bean 
+	  public Jackson2JsonDecoder jackson2JsonDecoder(ObjectMapper mapper) {
+	      return new Jackson2JsonDecoder(mapper);
+	  }
+
+	@Override
+	  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/**").addResourceLocations("classpath:META-INF/resources/");
+	  }
 }
